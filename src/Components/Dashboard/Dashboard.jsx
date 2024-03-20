32965ffcc0baffbba8d2b1446c7566b549c8c22f
madhusmita <madhusmita.p@eaglemountsoft.com>
2024-03-20 14:25:02 +0530

    add search field icons

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -5,6 +5,9 @@ import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 import img1 from "../../assets/Icons/home-icon.svg";
 import logoimg1 from "../../assets/Icons/video-icon.svg";
 import mask from "../../assets/Icons/Mask.svg";
+import Next from "../../assets/Icons/Next.svg";
+import Search from "../../assets/Icons/Search.svg";
+import mike from "../../assets/Icons/Mike.svg";
 import ListingCards from "./ListingCards";
 
 const Dashboard = () => {
@@ -26,7 +29,7 @@ const Dashboard = () => {
   };
 
   return (
-    <Container fluid className="dashboard p-md-4 p-sm-2">
+    <Container className="dashboard p-md-4 p-sm-2">
       <header className="d-flex justify-content-between">
         <h5>Select your Listing</h5>
         <div className="scroll-arrows">
@@ -62,21 +65,34 @@ const Dashboard = () => {
       <section className="search-section d-flex align-items-center justify-content-center pt-md-4">
         <Row>
           <Col xs={12} md={3}>
-            <div className="image-container text-md-end">
+            <div className="image-container">
               <img src={mask} alt="Images" className="mask-img" />
             </div>
           </Col>
-          <Col xs={12} md={9}>
-            <div className="search-container d-flex align-items-center justify-content-end">
+          <Col
+            xs={12}
+            md={9}
+            className="d-flex justify-content-center align-items-center"
+          >
+            <div className="search-container d-flex align-items-center justify-content-end position-relative">
               <Form>
                 <Form.Group controlId="searchInput">
                   <Form.Control
                     type="text"
-                    placeholder="Enter your search query"
+                    placeholder="Hi! I am freedo!How may i help you ?"
                     className="custom-search-input"
                   />
                 </Form.Group>
               </Form>
+              <div className="search-img position-absolute start-0 ms-4">
+                <img src={Search} alt="search-img" className="search-img" />
+              </div>
+              <div className="position-absolute end-0 me-5">
+                <img src={mike} alt="mike-img" className="mike-img" />
+              </div>
+              <div className="position-absolute end-0 me-3 opacity-md-2 p-md-3 rounded-4 next-img-div">
+                <img src={Next} alt="next-img" className="next-img" />
+              </div>
             </div>
           </Col>
         </Row>
