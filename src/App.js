6f8441a8b65317a546d853d2515b85fed95d7b91
Rahumathulla <if100al@gmail.com>
2024-03-21 15:20:42 +0530

    sidebar and header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import React from "react";
+import { React, useState } from "react";
 import Dashboard from "./Components/Dashboard/Dashboard";
+import Header from "./Components/Header";
+import Sidebar from "./Components/Sidebar";
+import "./App.css";
+import Home from "./Components/Home";
 
 const App = () => {
-  return <div>{/* <Dashboard /> */}</div>;
+
+  const [openSidebarToggle, setOpenSidebarToggle] = useState(false)
+
+  const OpenSidebar = () => {
+    setOpenSidebarToggle(!openSidebarToggle)
+  }
+
+  return (
+    <div className='grid-container'>
+      <Header OpenSidebar={OpenSidebar}/>
+      <Sidebar openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar}/>
+      <Home />
+    </div>
+  );
 };
 
 export default App;
