4f774aa86e84e57d12f572026c6cb251da9dafd0
madhusmita <madhusmita.p@eaglemountsoft.com>
2024-03-23 20:22:06 +0530

    Update home component

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import VenueHome from "./VenueForm/VenueHome";
 import Dashboard from "./Dashboard/Dashboard";
 
 const Home = () => {
   return (
-    // <Container>
-    <Dashboard />
-    // </Container>
+    <>
+      {/* <Dashboard /> */}
+      <VenueHome />
+    </>
   );
 };
 
